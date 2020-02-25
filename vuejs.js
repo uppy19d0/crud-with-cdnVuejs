b5901010fa69ce43e9ad23a777d2493faf5b5db9
Luis <luis_aneuris16@hotmail.com>
2020-02-25 10:28:29 -0400

    Crud Project

---
+++ vuejs.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+  new Vue({
+    el: '#appCruds',
+    data: {
+      nombre: '',
+      edad: '',
+      apellido: '',
+      birthday: '',
+      formActualizar: false,
+      idActualizar: -1,
+      nombreActualizar: '',
+      edadActualizar: '',
+      personas: []
+    },
+    methods: {
+      crearPersona: function() {
+        this.personas.push({
+          id: +new Date(),
+          nombre: this.nombre,
+          apellido: this.apellido,
+          edad: this.edad,
+          birthday: this.birthday
+        });
+        this.nombre = '';
+        this.apellido = '';
+        this.edad = '';
+        this.birthday = '';
+      },
+      verFormActualizar: function(persona_id) {
+        this.idActualizar = persona_id;
+        this.nombreActualizar = this.personas[persona_id].nombre;
+        this.apellidoActualizar = this.personas[persona_id].apellido;
+        this.edadActualizar = this.personas[persona_id].edad;
+        this.birthdayActualizar = this.personas[persona_id].birthday;
+        this.formActualizar = true;
+      },
+      borrarPersona: function(persona_id) {
+        this.personas.splice(persona_id, 1);
+      },
+      guardarActualizacion: function(persona_id) {
+        this.formActualizar = false;
+        this.personas[persona_id].nombre = this.nombreActualizar;
+        this.personas[persona_id].apellido = this.apellidoActualizar;
+        this.personas[persona_id].edad = this.edadActualizar;
+        this.personas[persona_id].birthday = this.birthdayActualizar;
+      }
+    }
+  });
+});
